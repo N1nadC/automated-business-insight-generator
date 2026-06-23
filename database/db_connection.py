e7b6b80f8dae63e3ee9dc0e187448f4f3ce209ad
N1nadC <ninadchaudhari57@gmail.com>
2026-06-23 23:18:32 +0530

    Phase 8 completed

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,14 +1,17 @@
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+import streamlit as st
+from sqlalchemy import create_engine
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = (
+    st.secrets["DATABASE_URL"]
+    if "DATABASE_URL" in st.secrets
+    else os.getenv("DATABASE_URL")
+)
 
 engine = create_engine(DATABASE_URL)
-
-print("Connected to PostgreSQL successfully!")
 
 def get_engine():
     return engine
